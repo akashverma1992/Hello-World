61f72b3ac07413942c8ca6a8cfad84fee2c3f8ef
Aakash Verma <aakash@Aakashs-MacBook-Air.local>
2018-03-02 15:56:03 +0530

    minor updates

--- background.js
+++ background.js
@@ -1,9 +1,20 @@
 // create window on app load
 chrome.app.runtime.onLaunched.addListener(() => {
   chrome.app.window.create("window.html", {
-    "outerBounds": {
-      "width": 400,
-      "height": 500
-    }
+    "id": 'MyWindowID',
+    "bounds": {
+      width: 800,
+      height: 600,
+      left: 100,
+      top: 100
+    },
+    minWidth: 800,
+    minHeight: 600
+  });
+});
+
+// onInstalled()
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.local.set({name: 'aakash'}, () => {
   });
 });
